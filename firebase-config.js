4cffe6cb49a4aed441d6536118849e3063f03b71
ranchod564 <159563372+ranchod564@users.noreply.github.com>
2024-02-09 08:15:06 -0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDp6QvffDerRrRpP6-BjFSmKJKNclgrPYk",
+  authDomain: "hbg-gdepkt.firebaseapp.com",
+  databaseURL: "https://hbg-gdepkt-default-rtdb.firebaseio.com",
+  projectId: "hbg-gdepkt",
+  storageBucket: "hbg-gdepkt.appspot.com",
+  messagingSenderId: "690725334568",
+  appId: "1:690725334568:web:0c1fb9f07611fc3af44f1d"
+};
